a2964e389917460e3f7a3bdbdafb185877639a5c
Sailesh Rijal <saileshrijal1@gmail.com>
2023-04-30 23:08:36 +0545

    frelaunce section added

--- src/components/CardFooter.vue
+++ src/components/CardFooter.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
+        <div class="text-center my-4 dark:text-white flex items-center justify-center">
+            Freelauncer at fiverr <a href="https://www.fiverr.com/r_sailesh" target="_blank"
+                class="text-white py-1 px-2 ms-2 rounded-md bg-blue-600">@r_sailesh</a>
+        </div>
         <div class="text-center">
             <a href="https://www.linkedin.com/in/saileshrijal" target="_blank"
                 class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
--- src/components/CardHeader.vue
+++ src/components/CardHeader.vue
@@ -14,6 +14,7 @@
             </li>
         </ul>
 
+
         <div>
             <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
         </div>
